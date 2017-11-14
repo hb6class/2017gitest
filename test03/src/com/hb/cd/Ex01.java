54ccb30bd0d7bec517daf654ec51de1a6e1a509e
hb06class <hb06class@gmail.com>
2017-11-14 17:52:21 +0900

    class dirgram

---
+++ test03/src/com/hb/cd/Ex01.java
@@ -0,0 +1,16 @@
+package com.hb.cd;
+
+public class Ex01 {
+
+	public int sabun = 0;
+	protected String name;
+	java.util.Date nalja;
+	private int pay;
+	
+	public void func01(){}
+	
+	private int func02(){
+		return 0;
+	}
+	protected void func03(int a){}
+}
